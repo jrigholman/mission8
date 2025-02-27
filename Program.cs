2db37dd2487b725ee1b84e8f50c5cce47bb4c5b4
lizzysimmons1 <153117350+lizzysimmons1@users.noreply.github.com>
2025-02-27 16:24:52 -0700

    All the merging

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Mission08_Group.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure Database Connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Optional: Add repository pattern (if needed in the future)
+// builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 
 var app = builder.Build();
 
@@ -9,7 +19,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
